3eaaae2b4432b6b6107d1b90056ca18afdc156ab
BrewDawg <Mike.Griffin@brewdawg.com>
2013-09-22 12:34:50 -0400

    Sample app so users can play around and set breakpoints and inspect the metadata

---
+++ TestApp/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ReadEdmxFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnV2 = new System.Windows.Forms.Button();
+            this.btnV3 = new System.Windows.Forms.Button();
+            this.lnkTiraggoEdmx = new System.Windows.Forms.LinkLabel();
+            this.lnkTiraggoEF = new System.Windows.Forms.LinkLabel();
+            this.codeWindow = new ScintillaNET.Scintilla();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.codeWindow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnV2
+            // 
+            this.btnV2.Location = new System.Drawing.Point(13, 12);
+            this.btnV2.Name = "btnV2";
+            this.btnV2.Size = new System.Drawing.Size(131, 28);
+            this.btnV2.TabIndex = 0;
+            this.btnV2.Text = "Load V2 EDMX File";
+            this.btnV2.UseVisualStyleBackColor = true;
+            this.btnV2.Click += new System.EventHandler(this.btnV2_Click);
+            // 
+            // btnV3
+            // 
+            this.btnV3.Location = new System.Drawing.Point(163, 12);
+            this.btnV3.Name = "btnV3";
+            this.btnV3.Size = new System.Drawing.Size(131, 28);
+            this.btnV3.TabIndex = 1;
+            this.btnV3.Text = "Load V3 EDMX File";
+            this.btnV3.UseVisualStyleBackColor = true;
+            this.btnV3.Click += new System.EventHandler(this.btnV3_Click);
+            // 
+            // lnkTiraggoEdmx
+            // 
+            this.lnkTiraggoEdmx.AutoSize = true;
+            this.lnkTiraggoEdmx.Location = new System.Drawing.Point(334, 20);
+            this.lnkTiraggoEdmx.Name = "lnkTiraggoEdmx";
+            this.lnkTiraggoEdmx.Size = new System.Drawing.Size(72, 13);
+            this.lnkTiraggoEdmx.TabIndex = 3;
+            this.lnkTiraggoEdmx.TabStop = true;
+            this.lnkTiraggoEdmx.Text = "Tiraggo.Edmx";
+            this.lnkTiraggoEdmx.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkTiraggoEdmx_LinkClicked);
+            // 
+            // lnkTiraggoEF
+            // 
+            this.lnkTiraggoEF.AutoSize = true;
+            this.lnkTiraggoEF.Location = new System.Drawing.Point(432, 20);
+            this.lnkTiraggoEF.Name = "lnkTiraggoEF";
+            this.lnkTiraggoEF.Size = new System.Drawing.Size(59, 13);
+            this.lnkTiraggoEF.TabIndex = 4;
+            this.lnkTiraggoEF.TabStop = true;
+            this.lnkTiraggoEF.Text = "Tiraggo.EF";
+            this.lnkTiraggoEF.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkTiraggoEF_LinkClicked);
+            // 
+            // codeWindow
+            // 
+            this.codeWindow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.codeWindow.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codeWindow.Location = new System.Drawing.Point(13, 48);
+            this.codeWindow.Name = "codeWindow";
+            this.codeWindow.Size = new System.Drawing.Size(588, 317);
+            this.codeWindow.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 382);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(565, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "In reality you would use \'TiraggoEdmx\' in your T4 Templates, See Tiraggo.EF which" +
+    " does just that.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 407);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.codeWindow);
+            this.Controls.Add(this.lnkTiraggoEF);
+            this.Controls.Add(this.lnkTiraggoEdmx);
+            this.Controls.Add(this.btnV3);
+            this.Controls.Add(this.btnV2);
+            this.Name = "Form1";
+            this.Text = "TiraggoEdmx.EF Demonstration by Mike Griffin";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.codeWindow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnV2;
+        private System.Windows.Forms.Button btnV3;
+        private System.Windows.Forms.LinkLabel lnkTiraggoEdmx;
+        private System.Windows.Forms.LinkLabel lnkTiraggoEF;
+        private ScintillaNET.Scintilla codeWindow;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TestApp/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TiraggoEdmx_v2;
+using TiraggoEdmx_v3;
+
+namespace ReadEdmxFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnV2_Click(object sender, EventArgs e)
+        {
+            codeWindow.Text = "";
+
+            TiraggoEdmx_v2.Edmx edmx_v2 = TiraggoEdmx_v2.Edmx.Load(@".\EdmxFiles\V2\Model1.edmx_");
+
+            // Get the information about the "Collaborator" table
+            TiraggoEdmx_v2.TiraggoEntityInfo v2_info = new TiraggoEdmx_v2.TiraggoEntityInfo(edmx_v2, "My2ndGenerationModel.CollaboratorInvite");
+
+            string attributes = "";
+
+            string text = "// Physical Table [" + v2_info.StorageInfo.Schema + "." + v2_info.StorageInfo.Name + "]" + Environment.NewLine;
+            text += "public partial class " + v2_info.ConceptualModel.Name + Environment.NewLine;
+            text += "{" + Environment.NewLine;
+
+            foreach (TiraggoEdmx_v2.tgColumnCLR prop in v2_info.ConceptualModel.Properties)
+            {
+                attributes = "";
+
+                // Determine Attributes
+                if (v2_info.IsInPrimaryKey(prop.Name))
+                {
+                    attributes += "\t[Key]" + Environment.NewLine;
+                }
+
+                if (prop.NullableSpecified && !prop.Nullable)
+                {
+                    attributes += "\t[Required]" + Environment.NewLine;
+                }
+
+
+                TiraggoEdmx_v2.tgProperty sqlInfo = v2_info.ColumnSQL[prop.Name];
+                text += "\t// Physical SQL Column [" + sqlInfo.Name + "], Type [" + sqlInfo.Type + "]" + Environment.NewLine;
+
+                if (!string.IsNullOrWhiteSpace(attributes))
+                {
+                    text += attributes;
+                }
+
+                text += "\tpublic " + prop.Type + " " + prop.Name + " { get; set; }" + Environment.NewLine + Environment.NewLine;
+            }
+
+            text += "}" + Environment.NewLine;
+
+            codeWindow.Text = text;
+        }
+
+        private void btnV3_Click(object sender, EventArgs e)
+        {
+            codeWindow.Text = "";
+
+            TiraggoEdmx_v3.Edmx edmx_v3 = TiraggoEdmx_v3.Edmx.Load(@".\EdmxFiles\V3\Model1.edmx_");
+
+            // Get the information about the "Collaborator" table
+            TiraggoEdmx_v3.TiraggoEntityInfo v3_info = new TiraggoEdmx_v3.TiraggoEntityInfo(edmx_v3, "My2ndGenerationModel.ForeignKey");
+
+            string attributes = "";
+
+            string text = "// Physical Table [" + v3_info.StorageInfo.Schema + "." + v3_info.StorageInfo.Name + "]" + Environment.NewLine;
+            text += "public partial class " + v3_info.ConceptualModel.Name + Environment.NewLine;
+            text += "{" + Environment.NewLine;
+
+            foreach (TiraggoEdmx_v3.tgColumnCLR prop in v3_info.ConceptualModel.Properties)
+            {
+                attributes = "";
+
+                // Determine Attributes
+                if (v3_info.IsInPrimaryKey(prop.Name))
+                {
+                    attributes += "\t[Key]" + Environment.NewLine;
+                }
+
+                if (prop.NullableSpecified && !prop.Nullable)
+                {
+                    attributes += "\t[Required]" + Environment.NewLine;
+                }
+
+
+                TiraggoEdmx_v3.tgProperty sqlInfo = v3_info.ColumnSQL[prop.Name];
+                text += "\t// Physical SQL Column [" + sqlInfo.Name + "], Type [" + sqlInfo.Type + "]" + Environment.NewLine;
+
+                if (!string.IsNullOrWhiteSpace(attributes))
+                {
+                    text += attributes;
+                }
+
+                text += "\tpublic " + prop.Type + " " + prop.Name + " { get; set; }" + Environment.NewLine + Environment.NewLine;
+            }
+
+            text += "}" + Environment.NewLine;
+
+            codeWindow.Text = text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            codeWindow.ConfigurationManager.Language = "cs";
+            codeWindow.ConfigurationManager.Configure();
+
+            LinkLabel.Link link = new LinkLabel.Link();
+	        link.LinkData = "http://brewdawg.github.io/Tiraggo.Edmx/";
+	        this.lnkTiraggoEdmx.Links.Add(link);
+
+            link = new LinkLabel.Link();
+	        link.LinkData = "http://brewdawg.github.io/Tiraggo.EF/";
+	        this.lnkTiraggoEF.Links.Add(link);
+        }
+
+        private void lnkTiraggoEF_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(e.Link.LinkData as string);
+        }
+
+        private void lnkTiraggoEdmx_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(e.Link.LinkData as string);
+        }
+    }
+}
